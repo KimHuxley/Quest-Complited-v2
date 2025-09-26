93209693e65ef4b02820dde6daeadd7b8cd9c49b
KimHuxley <wikikrywult@gmail.com>
2025-09-26 17:34:19 +0200

    add addon to github v4

--- QuestCompletionSound.lua
+++ QuestCompletionSound.lua
@@ -1,54 +1,54 @@
 local addonName = "QuestCompletionSound"
-local version = "1.0"
+local version = "1.1"
 
+-- Baza ustawień
 local QuestCompletionSoundDB = {
     sound = "Interface\\AddOns\\QuestCompletionSound\\sounds\\questcompleted.ogg"
 }
 
+-- Zapamiętane stany
 local previousObjectives = {}
 local previousCompleted = {}
 local isInitialScan = true
 
+-- Funkcja skanująca questy i ich cele
 local function ScanQuests()
     local newStates = {}
-    local newCompleted = {}
-    
-    -- Debug: Sprawdź, czy funkcja się wywołuje
+
+    -- Debug
     DEFAULT_CHAT_FRAME:AddMessage("|cFF00FFFF" .. addonName .. "|r: Scanning quests... Initial scan: " .. (isInitialScan and "Yes" or "No"))
-    
+
     for questIndex = 1, GetNumQuestLogEntries() do
         local title, level, tag, isHeader, isCollapsed, isComplete, frequency, questID = GetQuestLogTitle(questIndex)
-        
+
         if not isHeader then
             local id = questID or questIndex
-            
-            -- Debug: Informacje o queście
-            DEFAULT_CHAT_FRAME:AddMessage("|cFF00FFFF" .. addonName .. "|r: Quest [" .. (title or "Unknown") .. "] ID: " .. id .. ", isComplete: " .. (isComplete and "Yes" or "No"))
-            
+
+            -- Debug: stan questa
+            DEFAULT_CHAT_FRAME:AddMessage("|cFF00FFFF" .. addonName .. "|r: Quest [" .. (title or "Unknown") .. "] ID: " .. id .. ", isComplete: " .. tostring(isComplete))
+
             -- Sprawdź ukończenie całego questa
-            if isComplete and not previousCompleted[id] and not isInitialScan then
+            if (isComplete == 1 or isComplete == -1) and not previousCompleted[id] and not isInitialScan then
                 PlaySoundFile(QuestCompletionSoundDB.sound, "Master")
                 DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00" .. addonName .. "|r: Quest completed! (" .. (title or "Unknown") .. ")")
                 previousCompleted[id] = true
-            elseif isComplete then
+            elseif (isComplete == 1 or isComplete == -1) then
                 previousCompleted[id] = true
             end
-            
-            -- Sprawdź cele dla nieukończonych questów
-            if not isComplete then
-                newStates[id] = newStates[id] or {}
-                
+
+            -- Sprawdź cele questa
+            if not (isComplete == 1 or isComplete == -1) then
                 local numObjectives = GetNumQuestLeaderBoards(questIndex)
                 DEFAULT_CHAT_FRAME:AddMessage("|cFF00FFFF" .. addonName .. "|r: Quest [" .. (title or "Unknown") .. "] has " .. numObjectives .. " objectives")
-                
+
                 for objectiveIndex = 1, numObjectives do
                     local text, objectiveType, finished = GetQuestLogLeaderBoard(objectiveIndex, questIndex)
                     local key = id .. "-" .. objectiveIndex
                     newStates[key] = finished and true or false
-                    
-                    -- Debug: Informacje o celu
+
+                    -- Debug: cel questa
                     DEFAULT_CHAT_FRAME:AddMessage("|cFF00FFFF" .. addonName .. "|r: Objective [" .. (text or "Unknown") .. "] Finished: " .. (finished and "Yes" or "No"))
-                    
+
                     if newStates[key] and not previousObjectives[key] and not isInitialScan then
                         PlaySoundFile(QuestCompletionSoundDB.sound, "Master")
                         DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00" .. addonName .. "|r: Objective completed! (" .. (text or "Unknown") .. ")")
@@ -57,19 +57,27 @@ local function ScanQuests()
             end
         end
     end
-    
+
     previousObjectives = newStates
     isInitialScan = false
 end
 
+-- Główna ramka eventowa
 local frame = CreateFrame("Frame", addonName .. "Frame")
 frame:RegisterEvent("QUEST_LOG_UPDATE")
-frame:SetScript("OnEvent", function(self, event, ...)
+frame:RegisterEvent("QUEST_WATCH_UPDATE") -- Dodane: wykrywanie pełnego ukończenia questa
+frame:SetScript("OnEvent", function(self, event, arg1)
     if event == "QUEST_LOG_UPDATE" then
         ScanQuests()
+    elseif event == "QUEST_WATCH_UPDATE" then
+        local questID = arg1
+        local title = C_QuestLog and C_QuestLog.GetTitleForQuestID and C_QuestLog.GetTitleForQuestID(questID) or "Unknown"
+        PlaySoundFile(QuestCompletionSoundDB.sound, "Master")
+        DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00" .. addonName .. "|r: Quest fully completed! (" .. title .. ")")
     end
 end)
 
+-- Event ładowania
 local loadedFrame = CreateFrame("Frame")
 loadedFrame:RegisterEvent("VARIABLES_LOADED")
 loadedFrame:SetScript("OnEvent", function()
@@ -78,10 +86,10 @@ loadedFrame:SetScript("OnEvent", function()
     ScanQuests()
 end)
 
+-- Slash command
 SLASH_QCS1 = "/qcs"
 SlashCmdList["QCS"] = function(msg)
     DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00" .. addonName .. "|r: Loaded (" .. version .. ")")
-    -- Test dźwięku
     if msg == "test" then
         PlaySoundFile(QuestCompletionSoundDB.sound, "Master")
         DEFAULT_CHAT_FRAME:AddMessage("|cFF00FF00" .. addonName .. "|r: Test sound played")
